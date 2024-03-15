43871fe2c9ce9baa725eca23dc92e6541222aaee
Usama Muzammil <160235187+UsamaIT786@users.noreply.github.com>
2024-03-15 18:07:02 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+Repeat Exercise 4, but this time store the famous person’s name in a variable called
+famous_person. Then compose your message and store it in a new variable called message. Print your
+message.
+*/
+var famous_person = "Usama Muzammil";
+var message = "Are you using typescript ";
+console.log("".concat(famous_person, " once said,").concat(message));
---
+++ main.ts
@@ -0,0 +1,11 @@
+/*
+Repeat Exercise 4, but this time store the famous person’s name in a variable called
+famous_person. Then compose your message and store it in a new variable called message. Print your
+message.
+*/
+
+let famous_person : String="Usama Muzammil";
+
+let message : String= "Are you using typescript ";
+
+console.log(`${famous_person} once said,${message}`);
